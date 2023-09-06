ee892721baafa4939f0822d19a3b98c840283b5f
Moritz Gottschling <moritz@giantswarm.io>
2023-09-06 18:10:22 +0200

    Improve code and support two char keywords

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -38,60 +38,39 @@ func (l *Lexer) PeekChar() byte {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
-	for unicode.IsSpace(rune(l.ch)) {
+	l.skipSpaces()
+
+	tok, ok := token.FromTwoChars(l.ch, l.PeekChar())
+	if ok {
 		l.readChar()
+		l.readChar()
+		return tok
+	}
+	tok, ok = token.FromChar(l.ch)
+	if ok {
+		l.readChar()
+		return tok
+	}
+	if l.ch == 0 {
+		return token.Token{Type: token.EOF, Literal: ""}
 	}
 
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok.Literal = ""
-		tok.Type = token.EOF
-	default:
-		if isLetter(l.ch) {
-			word := l.readChars(isLetter)
+	if isLetter(l.ch) {
+		word := l.readChars(isLetter)
 
-			tok = token.FromWord(word)
+		tok = token.FromWord(word)
 
-		} else if isDigit(l.ch) {
-			digits := l.readChars(isDigit)
+	} else if isDigit(l.ch) {
+		digits := l.readChars(isDigit)
 
-			tok = token.FromDigits(digits)
-		} else {
-			tok = newToken(token.ILLEGAL, l.ch)
-		}
+		tok = token.FromDigits(digits)
+	} else {
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 
 	l.readChar()
 
 	return tok
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
-func isLetter(ch byte) bool {
-	return unicode.IsLetter(rune(ch)) || ch == '_'
-}
-
-func isDigit(ch byte) bool {
-	return unicode.IsDigit(rune(ch))
 }
 
 func (l *Lexer) readChars(isPartOfWord func(byte) bool) string {
@@ -109,4 +88,22 @@ func (l *Lexer) readChars(isPartOfWord func(byte) bool) string {
 	word += string(l.ch)
 
 	return word
+}
+
+func (l *Lexer) skipSpaces() {
+	for unicode.IsSpace(rune(l.ch)) {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return unicode.IsLetter(rune(ch)) || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return unicode.IsDigit(rune(ch))
 }
--- token/token.go
+++ token/token.go
@@ -2,29 +2,6 @@ package token
 
 type TokenType string
 
-type Token struct {
-	Type    TokenType
-	Literal string
-}
-
-func FromWord(word string) Token {
-	var tokenType TokenType
-	switch word {
-	case "fn":
-		tokenType = FUNCTION
-	case "let":
-		tokenType = LET
-
-	default:
-		tokenType = IDENT
-	}
-	return Token{Type: tokenType, Literal: word}
-}
-
-func FromDigits(digits string) Token {
-	return Token{Type: INT, Literal: digits}
-}
-
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -65,3 +42,68 @@ const (
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
 )
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var singleCharTokens = map[byte]TokenType{
+	'=': ASSIGN,
+	'+': PLUS,
+	'-': MINUS,
+	'!': BANG,
+	'*': ASTERISK,
+	'/': SLASH,
+	'<': LT,
+	'>': GT,
+	'(': LPAREN,
+	')': RPAREN,
+	'{': LBRACE,
+	'}': RBRACE,
+	',': COMMA,
+	';': SEMICOLON,
+}
+
+func FromChar(char byte) (Token, bool) {
+	if tokenType, ok := singleCharTokens[char]; ok {
+		return Token{Type: tokenType, Literal: string(char)}, true
+	}
+	return Token{}, false
+}
+
+var twoCharTokens = map[string]TokenType{
+	"==": EQ,
+	"!=": NOT_EQ,
+}
+
+func FromTwoChars(a, b byte) (Token, bool) {
+	if tokenType, ok := twoCharTokens[string(a)+string(b)]; ok {
+		return Token{Type: tokenType, Literal: string(a) + string(b)}, true
+	}
+	return Token{}, false
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func FromWord(word string) Token {
+	var tokenType TokenType = IDENT
+	for k, v := range keywords {
+		if k == word {
+			tokenType = v
+		}
+	}
+	return Token{Type: tokenType, Literal: word}
+}
+
+func FromDigits(digits string) Token {
+	return Token{Type: INT, Literal: digits}
+}
